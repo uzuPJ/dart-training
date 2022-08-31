bf56b79f151d3f529aaa5503235d91f2dadb8e3d
Aitaro Chaya <aitaro.chaya@gmail.com>
2022-08-31 17:14:04 +0900

    chore: add example

--- at-coder/1_practice_a.dart
+++ at-coder/1_practice_a.dart
@@ -3,6 +3,14 @@ import 'package:test/test.dart';
 // 問題文
 // https://atcoder.jp/contests/abs/tasks/practice_1
 
+// 例) input のデータを変数に格納する
+// final input_list = input.split('\n');
+// int a = int.parse(input_list[0]);
+// final l2 = input_list[1].split(' ');
+// int b = int.parse(l2[0]);
+// int c = int.parse(l2[1]);
+// final s = input_list[2];
+
 String solver(String input) {
   // 実装してください！
   return "";
