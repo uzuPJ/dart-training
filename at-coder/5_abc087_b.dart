d4f77ab7f05d4951ab6c52d22496e41df68d6d04
Kentaro Tanaka <yukimura-ken2003@g.ecc.u-tokyo.ac.jp>
2022-09-03 15:55:30 +0900

    feat: 問題５完了

--- at-coder/5_abc087_b.dart
+++ at-coder/5_abc087_b.dart
@@ -5,7 +5,30 @@ import 'package:test/test.dart';
 
 String solver(String input) {
   // 実装してください！
-  return "";
+
+  var ans = 0;
+
+  final inputList = input.split('\n');
+  final A = int.parse(inputList[0]);
+  final B = int.parse(inputList[1]);
+  final C = int.parse(inputList[2]);
+  final N = int.parse(inputList[3]);
+
+  var a = 0;
+  var b = 0;
+  var c = 0;
+
+  for (a = 0; a <= A; a++) {
+    for (b = 0; b <= B; b++) {
+      for (c = 0; c <= C; c++) {
+        if (500 * a + 100 * b + 50 * c == N) {
+          ans++;
+        }
+      }
+    }
+  }
+
+  return "$ans";
 }
 
 // 以下テストコード
