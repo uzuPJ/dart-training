3a49d8d85d995a21556611688c58949b62d4060b
Kentaro Tanaka <yukimura-ken2003@g.ecc.u-tokyo.ac.jp>
2022-09-05 01:05:17 +0900

    feat: 問題７完了

--- at-coder/7_abc088_b.dart
+++ at-coder/7_abc088_b.dart
@@ -1,11 +1,57 @@
 import 'package:test/test.dart';
+import 'dart:math';
 
 // 問題文
 // https://atcoder.jp/contests/abs/tasks/abc088_b
 
 String solver(String input) {
   // 実装してください！
-  return "";
+  final inputList = input.split('\n');
+  final N = int.parse(inputList[0]);
+  var cards = inputList[1].split(' ').map(int.parse).toList();
+  var ans = 0;
+
+  var AliceSum = 0;
+  var BobSum = 0;
+
+  // var isAliceTurn = true;
+
+  // while (!cards.isEmpty) {
+  //   var maxCard = cards.reduce(max);
+  //   if (isAliceTurn) {
+  //     AliceSum = AliceSum + maxCard;
+  //   } else {
+  //     BobSum = BobSum + maxCard;
+  //   }
+  //   cards.remove(maxCard);
+  //   isAliceTurn = !isAliceTurn;
+  // }
+
+  while (true) {
+    var maxCard = cards.reduce(max);
+    if (cards.length > 0) {
+      AliceSum = AliceSum + maxCard;
+      cards.remove(maxCard);
+    }
+
+    if (cards.isEmpty) {
+      break;
+    }
+
+    maxCard = cards.reduce(max);
+    if (cards.length > 0) {
+      BobSum = BobSum + maxCard;
+      cards.remove(maxCard);
+    }
+
+    if (cards.isEmpty) {
+      break;
+    }
+  }
+
+  ans = AliceSum - BobSum;
+
+  return "$ans";
 }
 
 // 以下テストコード
